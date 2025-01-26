b2797418f509215aa2c09bf9233447e994949d75
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2025-01-26 23:18:13 +0700

    a more complete jadwal generation

--- src/cli/convertJadwalV2.ts
+++ src/cli/convertJadwalV2.ts
@@ -243,6 +243,236 @@ await Promise.all(
     })
 );
 
+// Add missing data from original jadwal into the newJadwal
+
+function guessMatkulSks(classDurationPair: [string, number][]) {
+    // Guess the SKS of a matkul based on the duration of its classes
+    // Put into bins based on the class label
+
+    const bins = new Map<string, number[]>();
+    classDurationPair.forEach(([label, duration]) => {
+        if (!bins.has(label)) bins.set(label, []);
+        bins.get(label)?.push(duration);
+    });
+
+    // If each class only contains one schedule, find median and calculate from that.
+    // If each class contains more than one schedule, add all the durations and divide by 2.
+    // This assumes that the subject has a practical class.
+    const values = Array.from(bins.values()).map((durations) => {
+        if (durations.length === 1) return durations[0];
+        return durations.reduce((a, b) => a + b, 0) / 2;
+    });
+
+    values.sort((a, b) => a - b);
+    const median = values[Math.floor(values.length / 2)];
+
+    // Guess the SKS based on the median duration
+    // Some units like to use 60 minutes per SKS, while some like to do the proper 50 minutes per SKS.
+    // If evenly divisible by 50, then we use 50, else we use 60 rounded down.
+    if (median % 50 === 0) return Math.floor(median / 50);
+    return Math.floor(median / 60);
+}
+
+const unitPrefixes = new Map<string, number>();
+function guessUnitPrefix(namaUnit: string) {
+    // Unit prefix is a two digit uppercase letter that is used to identify a unit
+    // This function guesses the unit prefix based on the unit name
+    // Duplicates are not allowed
+    // First attempt: use first letters of the first two words
+    // Second attempt: use first two letters of the unit name
+    // If both attempts resulted in a collision, then throw an error.
+
+    const firstAttempt =
+        "~" +
+        namaUnit
+            .split(" ")
+            .slice(0, 2)
+            .map((word) => word[0].toUpperCase())
+            .join("");
+    const secondAttempt = "~" + namaUnit.slice(0, 2).toUpperCase();
+
+    if (firstAttempt.length === 3 && !unitPrefixes.has(firstAttempt)) {
+        unitPrefixes.set(firstAttempt, 1);
+        return firstAttempt;
+    }
+
+    if (secondAttempt.length === 3 && !unitPrefixes.has(secondAttempt)) {
+        unitPrefixes.set(secondAttempt, 1);
+        return secondAttempt;
+    }
+
+    // throw new Error(`Unit prefix collision for unit "${namaUnit}"`);
+    return secondAttempt;
+}
+
+// Add all existing unit prefixes to the map
+newJadwal.forEach((matkul) => {
+    const prefix = matkul.kode.slice(0, 2);
+    unitPrefixes.set(prefix, 1);
+});
+
+// Find duplicate mata kuliah
+function findDuplicate(unit: string, nama: string, kode: string) {
+    let matchedMatkul = newJadwal.find(
+        (mk) => mk.unit === unit && mk.nama === nama
+    );
+
+    if (!matchedMatkul) {
+        matchedMatkul = newJadwal.find((mk) => mk.kode === kode);
+    }
+
+    return matchedMatkul;
+}
+
+// Loop over the old schedule. If no duplicate is found, then add it to the new schedule.
+await Promise.all(
+    Object.values(jadwal).map(async (unit) => {
+        // ignore any unit which name contains "(*)"
+        if (unit.unit.includes("(*)")) return;
+
+        // ignore any unit with name "magister"
+        if (unit.unit.toLowerCase().includes("magister")) return;
+
+        // ignore any unit with empty jadwal
+        if (Object.keys(unit.jadwal).length === 0) return;
+
+        const unitPrefix = guessUnitPrefix(unit.unit);
+
+        Object.entries(unit.jadwal).forEach(([namaMatkul, matkul]) => {
+            // Check duplicate
+            const duplicate = findDuplicate(unit.unit, namaMatkul, unitPrefix);
+            if (duplicate) return;
+
+            console.log(
+                `${namaMatkul} is present in old, adding to new with guessed SKS and kode`
+            );
+
+            const sks = guessMatkulSks(
+                matkul.kuliah.map((kelas) => [
+                    kelas.kelas,
+                    kelas.lengthMinutes ?? 0,
+                ])
+            );
+
+            const kode = `${unitPrefix}${unitPrefixes
+                .get(unitPrefix)
+                ?.toString()
+                .padStart(3, "0")}`;
+
+            const semester: null | number = null;
+
+            // increment unit prefix counter
+            unitPrefixes.set(
+                unitPrefix,
+                (unitPrefixes.get(unitPrefix) ?? 1) + 1
+            );
+
+            // Remove double spaces in namaMatkul
+            const cleanedNamaMatkul = namaMatkul.replaceAll("  ", " ");
+
+            const newMatkul: MataKuliah = {
+                prsId: "",
+                nama: cleanedNamaMatkul,
+                semester,
+                sks,
+                unit: unit.unit,
+                kode,
+                kelas: [],
+            };
+
+            matkul.kuliah.forEach((kelas) => {
+                // skip if jam mulai is null
+                if (kelas.jamMulai === null) return;
+
+                // skip if ruang is /
+                if (kelas.ruang === "/") return;
+
+                // find kelas or append new kelas
+                const findKelas = newMatkul.kelas.find(
+                    (k) => k.kelas === kelas.kelas
+                );
+                const newKelas =
+                    findKelas ??
+                    ({
+                        kelas: kelas.kelas,
+                        jadwal: [] as JadwalMataKuliah[],
+                        jadwalUts: [] as JadwalUjianMataKuliah[],
+                        jadwalUas: [] as JadwalUjianMataKuliah[],
+                    } satisfies KelasMataKuliah);
+
+                if ("catatan" in kelas && typeof kelas.catatan === "string") {
+                    newKelas.keterangan = kelas.catatan;
+                }
+
+                // append jadwal
+                newKelas.jadwal.push({
+                    dayOfWeek:
+                        hariToDayOfWeek[
+                            kelas.hari as keyof typeof hariToDayOfWeek
+                        ],
+                    startHour: kelas.jamMulai,
+                    startMinute: kelas.menitMulai,
+                    durasi: kelas.lengthMinutes ?? 0,
+                    ruang: kelas.ruang,
+                });
+
+                if (!findKelas) newMatkul.kelas.push(newKelas);
+            });
+
+            // Do the same for UTS and UAS
+            matkul.uts.forEach((kelas) => {
+                // find kelas or append new kelas
+                const findKelas = newMatkul.kelas.find(
+                    (k) => k.kelas === kelas.kelas
+                );
+                const newKelas =
+                    findKelas ??
+                    ({
+                        kelas: kelas.kelas,
+                        jadwal: [] as JadwalMataKuliah[],
+                        jadwalUts: [] as JadwalUjianMataKuliah[],
+                        jadwalUas: [] as JadwalUjianMataKuliah[],
+                    } satisfies KelasMataKuliah);
+
+                // append jadwal
+                newKelas.jadwalUts.push({
+                    date: new Date(kelas.date ?? 0),
+                    lengthMinutes: kelas.lengthMinutes,
+                    ruang: kelas.ruang,
+                });
+
+                if (!findKelas) newMatkul.kelas.push(newKelas);
+            });
+
+            matkul.uas.forEach((kelas) => {
+                // find kelas or append new kelas
+                const findKelas = newMatkul.kelas.find(
+                    (k) => k.kelas === kelas.kelas
+                );
+                const newKelas =
+                    findKelas ??
+                    ({
+                        kelas: kelas.kelas,
+                        jadwal: [] as JadwalMataKuliah[],
+                        jadwalUts: [] as JadwalUjianMataKuliah[],
+                        jadwalUas: [] as JadwalUjianMataKuliah[],
+                    } satisfies KelasMataKuliah);
+
+                // append jadwal
+                newKelas.jadwalUas.push({
+                    date: new Date(kelas.date ?? 0),
+                    lengthMinutes: kelas.lengthMinutes,
+                    ruang: kelas.ruang,
+                });
+
+                if (!findKelas) newMatkul.kelas.push(newKelas);
+            });
+
+            newJadwal.push(newMatkul);
+        });
+    })
+);
+
 // Sort the array by "nama" field for consistency
 newJadwal.sort((a, b) => a.nama.localeCompare(b.nama));
 
